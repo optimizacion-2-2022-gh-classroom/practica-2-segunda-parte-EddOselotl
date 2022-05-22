b4e716400de6fcd26719b42bd22073dd66293d28
EddOselotl <ing.edbaz@gmail.com>
2022-05-22 11:53:44 -0500

    Se sube archivo setup.py con la nueva forma de compilar para cythonizer y compilar nuestro paquete issues #3

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,10 @@
 #see: https://setuptools.readthedocs.io/en/latest/userguide/package_discovery.html
 from setuptools import setup, find_packages
+import numpy as np
+from Cython.Build import cythonize
 
 setup(name="MaxFlowAeiu",
-      version="0.1.2",
+      version="0.1.3",
       description=u"Execution of the Fork Fulkerson method to find the maximum flow of a network",
       long_description=open("README.md", "r", encoding="utf-8").read(),
       url="https://github.com/optimizacion-2-2022-gh-classroom/practica-2-primera-parte-urieluard",
@@ -15,4 +17,9 @@ setup(name="MaxFlowAeiu",
       license="MIT",
       keywords='max flow',
       packages=find_packages(),
+      ext_modules = cythonize("MaxFlowAeiu/MaxFlowAeiu.pyx",compiler_directives={'language_level' : 3}),
+      include_dirs=np.get_include(),
+      install_requires=[
+        'numpy>=1.21.5'
+        ]
       )
